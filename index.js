114bb34a2a662e80a4dabdd55bc6f10005a2a24c
Raviikumar001 <samcollins9899@gmail.com>
2021-08-29 00:04:30 +0530

    added javascript logic

--- index.js
+++ index.js
@@ -4,21 +4,45 @@ const luckyNumber = document.querySelector('#lucky-number');
 
 const CheckNumberButton =   document.querySelector('#check-number');
 
+const outputBox = document.querySelector('#output-box');
+function compareValues(sum,luckyNumber)
+{
+   if(sum%luckyNumber===0){
+       outputBox.innerText= "Your birthday is lucky 🔥🔥";
+   }else {
+       outputBox.innerText= "Your birthday is not lucky. 🤣It's just for fun.";
+   }
+}
+
+function checkBirthDateisLucky(){
+    const dob = dateofBirth.value;
+    const sum = calculateSum(dob);
+    if(sum && dob)
+    compareValues(sum, luckyNumber.value);
+    else
+    outputBox.innerText ="Please enter in both the details.";
+    
+}
+
+
+
 function checkNumberisLucky(){
     const dob= dateofBirth.value;
     const sum= calculateSum(dob);
+    console.log(sum);
 }
 
-function calculateSum(date)
+function calculateSum(dob)
 {
     dob= dob.replaceAll("-","");
     let sum = 0;
     for(let index=0; index<dob.length; index++){
-        sum = sum + dob.charAt(index);
+        sum = sum + Number(dob.charAt(index));
+
         }
+        return sum;
 }
 
 
-CheckNumberButton.addEventListener('click',function getvalues(){
-    console.log(dateofBirth.value,luckyNumber.value);
-})
+CheckNumberButton.addEventListener('click',checkBirthDateisLucky);
+    
